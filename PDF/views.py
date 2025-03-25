90d9047d4941c855c7564ca903e868839d24c37b
Dineth <dinethfiverr23@gmail.com>
2025-03-25 11:27:14 +0530

    Building Views For App

--- PDF/views.py
+++ PDF/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import ProfileInfo
 
 # Create your views here.
+
+
+def index(request):
+    """View for the home page"""
+
+    return render(request, 'pdf/accept.html')
