476fdae7bdb5cab1e54adeeb14ae66864e81efeb
Dineth <dinethfiverr23@gmail.com>
2025-03-25 11:42:42 +0530

    Get Input Data An POST Method And Save The DataBase

--- PDF/views.py
+++ PDF/views.py
@@ -7,4 +7,23 @@ from .models import ProfileInfo
 def index(request):
     """View for the home page"""
 
+    if request.method == 'POST':
+
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        summary = request.POST.get('summary', '')
+        degree = request.POST.get('degree', '')
+        school = request.POST.get('school', '')
+        university = request.POST.get('university', '')
+        previous_work = request.POST.get('previous_work', '')
+        skills = request.POST.get('skills', '')
+
+        profile = ProfileInfo(name=name, email=email, phone=phone, summary=summary, degree=degree, 
+                              school=school, university=university, previous_work=previous_work, skills=skills)
+        profile.save()
+
+
+
+
     return render(request, 'pdf/accept.html')
